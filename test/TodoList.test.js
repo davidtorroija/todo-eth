e38878a2055dcc2e4327f02485d12b5e9538b3ff
David <davidtorroija@gmail.com>
2022-04-24 09:30:23 -0500

    adding more tests

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -14,4 +14,23 @@ contract('TodoList', (accounts) => {
         expect(address).to.not.equal(undefined);
         expect(address).to.not.equal('');
     })
+
+    it('contract succesfully deployed!', async () => {
+        const address = await this.todoList.address
+        // expect(accounts).toContain(this.todolist.address)
+        expect(address).to.not.equal(0x0);
+        expect(address).to.not.equal(null);
+        expect(address).to.not.equal(undefined);
+        expect(address).to.not.equal('');
+    })
+
+    it('todo list tasks', async () => {
+        const taskCount = await this.todoList.taskCount()
+        const task = await this.todoList.tasks(taskCount)
+        expect(task.id.toNumber()).to.equal(taskCount.toNumber())
+        expect(task.content).to.equal("1. become a crypto developer")
+        expect(task.completed).to.equal(false)
+        expect(task.id.toNumber()).to.equal(1)
+
+    })
 })
